67dc4d164d4f5fc15a3ff5d065aed879752f42ad
reegodev <reegodev@gmail.com>
2020-03-07 20:46:58 -1000

    Form bug fix pt2

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -24,7 +24,7 @@ const Page = ({ data }) => {
         <div className='formBreak'></div>
         <div className='mainForm'>
             {/* action='/enviado'*/}
-            <form className='serviceBoxWide' method='post' name='mensagems' data-netlify="true">{/* data-netlify-honeypot="bot-field">*/}
+            <form className='serviceBoxWide' method='post' name='mensagems' netlify>{/* data-netlify-honeypot="bot-field">*/}
                 {/*<input type="hidden" name="bot-field"/>*/}
                 <input type="hidden" name="form-name" value="mensagems" />
                 <h1>Contato</h1>
