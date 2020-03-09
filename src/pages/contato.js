9296973718e799f897673eadb3e6d3808f6d4a74
reegodev <reegodev@gmail.com>
2020-03-08 14:39:39 -1000

    AJAX form submission

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -7,40 +7,132 @@ import SEO from '../components/seo';
 
 import Layout from '../components/layout';
 
-const Page = ({ data }) => {
-    const frontmatter = data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-    const displayImage = frontmatter['contactDisplayImage'];
-    const text = frontmatter['contactText'];
-
-    return (
-    <div className='mainContent'>
-        <div className='formDescription'>
-            <div className='formDescriptionImage' style={{background:"url('" + displayImage + "')", backgroundSize:'cover'}}></div>
-        </div>
-        <div className='formBreak'></div>
-        <h1 className='contentTitle'>Fale connosco</h1>
-        <p className='contentDescription'>{ text }</p>
-        <div className='formBreak'></div>
-        <div className='mainForm'>
-            {/* action='/enviado'*/}
-            <form className='serviceBoxWide' method='post' action='/enviado' name='mensagems' data-netlify-honeypot="bot-field" netlify>
-                <input type="hidden" name="bot-field"/>
-                <input type="hidden" name="form-name" value="mensagems" />
-                <h1>Contato</h1>
-                <div className='formBreakSmall'></div>
-                <p className='formText'>Email</p>
-                <input type='email' name='email' className='textInput' placeholder='email' required/>
-                <div className='formBreak'></div>
-                <p className='formText'>Mensagem</p>
-                <textarea name='conteudo' rows='5' placeholder='mensagem'></textarea>
-                <div className='formBreak'></div>
-                <input className='submitButton' type='submit' value='Submeter'/>
-            </form>
-        </div>
-    </div>
-    );
+const SuccessRedirect = () => {
+  useEffect(() => {
+    navigate('/enviado');
+  }, []);
+  return null;
 };
+
+
+class Page extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'redirect': false
+        };
+    }
+
+    render() {
+        data = this.props.data
+
+            const frontmatter = data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
+
+        const displayImage = frontmatter['contactDisplayImage'];
+        const text = frontmatter['contactText'];
+
+        if(this.state.redirect) {
+            return SuccessRedirect();
+        }
+
+        return (
+        <div className='mainContent'>
+            <div className='formDescription'>
+                <div className='formDescriptionImage' style={{background:"url('" + displayImage + "')", backgroundSize:'cover'}}></div>
+            </div>
+            <div className='formBreak'></div>
+            <h1 className='contentTitle'>Fale connosco</h1>
+            <p className='contentDescription'>{ text }</p>
+            <div className='formBreak'></div>
+            <div className='mainForm'>
+                {/* action='/enviado'*/}
+                <form onSubmit={(e)=>this.formSubmission.bind(this)} className='serviceBoxWide' method='post' action='/enviado' name='mensagems' data-netlify-honeypot="bot-field" netlify>
+                    <input type="hidden" name="bot-field"/>
+                    <input type="hidden" name="form-name" value="mensagems" />
+                    <h1>Contato</h1>
+                    <div className='formBreakSmall'></div>
+                    <p className='formText'>Email</p>
+                    <input type='email' name='email' className='textInput' placeholder='email' required/>
+                    <div className='formBreak'></div>
+                    <p className='formText'>Mensagem</p>
+                    <textarea name='conteudo' rows='5' placeholder='mensagem'></textarea>
+                    <div className='formBreak'></div>
+                    <input className='submitButton' type='submit' value='Submeter'/>
+                </form>
+            </div>
+        </div>
+        );
+    }
+
+    formSubmission(e) {
+
+        e.preventDefault();
+
+        const elements = e.target.elements;
+
+        const form = {
+            'bot-field': elements['bot-field'].value,
+            'form-name': elements['form-name'].value,
+            'email': elements['email'].value,
+            'mensagem': elements['mensagem'].value,
+        }
+
+        await fetch({
+            'method':'POST',
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode(form)
+        })
+            .then(() => {
+                this.setState(
+                {
+                    'redirect': true,
+                })
+            })
+            .catch(err => {
+
+            });
+    }
+}
+
+// const Page = ({ data }) => {
+//     const frontmatter = data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
+
+//     const displayImage = frontmatter['contactDisplayImage'];
+//     const text = frontmatter['contactText'];
+
+//     return (
+//     <div className='mainContent'>
+//         <div className='formDescription'>
+//             <div className='formDescriptionImage' style={{background:"url('" + displayImage + "')", backgroundSize:'cover'}}></div>
+//         </div>
+//         <div className='formBreak'></div>
+//         <h1 className='contentTitle'>Fale connosco</h1>
+//         <p className='contentDescription'>{ text }</p>
+//         <div className='formBreak'></div>
+//         <div className='mainForm'>
+//             {/* action='/enviado'*/}
+//             <form className='serviceBoxWide' method='post' action='/enviado' name='mensagems' data-netlify-honeypot="bot-field" netlify>
+//                 <input type="hidden" name="bot-field"/>
+//                 <input type="hidden" name="form-name" value="mensagems" />
+//                 <h1>Contato</h1>
+//                 <div className='formBreakSmall'></div>
+//                 <p className='formText'>Email</p>
+//                 <input type='email' name='email' className='textInput' placeholder='email' required/>
+//                 <div className='formBreak'></div>
+//                 <p className='formText'>Mensagem</p>
+//                 <textarea name='conteudo' rows='5' placeholder='mensagem'></textarea>
+//                 <div className='formBreak'></div>
+//                 <input className='submitButton' type='submit' value='Submeter'/>
+//             </form>
+//         </div>
+//     </div>
+//     );
+// };
 
 export default ({ data }) => (
     <Layout>
